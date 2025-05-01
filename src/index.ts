282f2d1ca91bc280c23d67e7e2634ec681aca12c
Lili <you@example.com>
2025-04-30 23:29:29 -0400

    add api

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { setupApp } from "./setup-app";
  
 const app = express();
 setupApp(app);
-
-const PORT = process.env.PORT || 3000;
+export default app;
+// const PORT = process.env.PORT || 3000;
 
 //@ts-ignore
 //app.get("/", (req, res) => res.send("Express on Vercel"));
 // запуск приложения
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`);
+// });
